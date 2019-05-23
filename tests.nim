b04120653c06d09569d0cf1f71eea39eea998f9c
Vindaar <basti90@gmail.com>
2019-05-23 16:14:26 +0200

    add `Formula`, creation via `~` like in R as macro / template magic
    We can parse (or let the Nim parser) a formula starting with `~` and
    store it in a `FormulaNode`, which we can serialize at will to perform
    the calculation stored in it.
    
    The syntax works just like in R (at least it should). Note however,
    that the arguments to the formula *MUST* be untyped
    identifiers. Support for variable injection etc. is not given. In that
    case use the (not yet implemented) string or macro (using `{}`)
    version of this.
    
    I guess...

---
+++ tests.nim
@@ -0,0 +1,29 @@
+import unittest
+import ggplotnim
+
+
+test "Testing ~ formula creation":
+  let f = x ~ y
+  let a = x ~ (a - b)
+  let g = n ~ m + a * b * d
+  let g2 = n ~ m + a - b + d
+  let g3 = n ~ m + a * b / d
+  let single = ~ x
+  let gg1 = hwy ~ (displ + cyl - cty)
+  let gg2 = hwy ~ displ + cyl - cty
+
+  doAssert $f == "(~ x y)"
+  doAssert $a == "(~ x (- a b))"
+  doAssert $g == "(~ n (+ m (* (* a b) d)))"
+  doAssert $g2 == "(~ n (+ (- (+ m a) b) d))"
+  doAssert $g3 == "(~ n (+ m (/ (* a b) d)))"
+  doAssert $single == "(~  x)"  # NOTE: 2 spaces, since LHS is empty
+  doAssert $gg1 == "(~ hwy (- (+ displ cyl) cty))"
+  doAssert $gg2 == "(~ hwy (- (+ displ cyl) cty))"
+
+test "Serializing ~ formula":
+  let mpg = readCsv("data/mpg.csv")
+  let f = hwy ~ (displ + cyl - cty) # this doesn't make sense, but anyways...
+  # Displacement + Cylinders - City mpg. Yeah :D
+  # use RHS of formula for calculation of 0 row.
+  doAssert f.rhs.serialize(mpg, 0) == -12.2
