8657d0cc2c0a6b7776334b6003c5d777e77845cd
Vindaar <basti90@gmail.com>
2019-05-29 09:17:18 +0200

    refactor `ggsave` into geom parts, introduce Scales
    Refactors the `ggsave` part for now into different procs for the
    different `geoms`.
    Also introduces the usage of `Scales`. Currently scales is still a
    normal object, but needs to become a variant object. Also we currently
    assume that the colormap is element 1 of the `Scales` sequence. Must
    be changed :)

--- ggplotnim.nim
+++ ggplotnim.nim
@@ -18,8 +18,19 @@ type
     color: Option[string] # classify by color
     size: Option[string] # classify by size
 
+  # TODO: should not one scale belong to only one axis?
+  # But if we do that, how do we find the correct scale in the seq[Scales]?
+  # Replace seq[Scales] by e.g. Table[string, Scales] where string is some
+  # static identifier we can calculate to retrieve it?
+  # e.g. `xaxis`, `<name of geom>.xaxis` etc.?
   Scales = object
-    discard
+    numXTicks: int
+    numYTicks: int
+    # scale represented by grouping string classes by colors
+    colorMap: OrderedTable[string, Color]
+    # stores the data for the color column so that we can get the
+    # color class for each element
+    colors: seq[string]
 
   Facet = object
     discard
@@ -28,7 +39,7 @@ type
     fname: string
 
   GeomKind = enum
-    gkPoint, gkBar, gkHistogram, gkFreqPoly
+    gkPoint, gkBar, gkHistogram, gkFreqPoly, gkTile
   Geom = object
     style: Option[Style] # if set, apply this style instead of parent's
     aes: Aesthetics # a geom can have its own aesthetics. Needs to be part of
@@ -50,9 +61,47 @@ type
     facets: seq[Facet]
     geoms: seq[Geom]
 
+proc getColor[T: SomeInteger | string](s: Scales, k: T): Color =
+  ## returns the color from the `Scales` based on either a key
+  ## as a string or as an index
+  # doAssert s.kind == skColor
+  when T is SomeInteger:
+    # return the color corresponding to row `k` of the
+    # color column
+    result = s.colorMap[s.colors[k]]
+  else:
+    # return the color corresponding to class `k`
+    result = s.colorMap[k]
+
+proc getColorKey(s: Scales, at: int): string =
+  ## returns the key of the colorMap at insertion index `at`
+  ## This works because `colorMap` is an OrderedTable
+  result = toSeq(keys(s.colorMap))[at]
+
+proc addAes(p: var GgPlot, aes: Aesthetics) =
+  ## adds the aesthetics to the plot. This is non trivial, because
+  ## an aestetics encodes information that may have to be calculated
+  if aes.color.isSome:
+    # get the column by which we want to color
+    let colors = p.data.dataTo(aes.color.get, string)
+    # convert to set to filter duplicates, back to seq and sort
+    let catSeq = colors.toSet.toSeq.sorted
+    let colorCs = ggColorHue(catSeq.len)
+    var cScale = Scales()
+    cScale.colors = colors
+    for i, k in catSeq:
+      cScale.colorMap[k] = colorCs[i]
+    p.scales.add cScale
+  # finally add the actual aesthetics
+  p.aes.add aes
+
 proc ggplot*[T](data: T, aes: Aesthetics): GgPlot[T] =
+  let defaultScale = Scales(numXTicks: 10,
+                            numYTicks: 10)
   result = GgPlot[T](data: data,
-                     aes: @[aes])
+                     scales: @[defaultScale])
+  result.addAes aes
+  echo "GG HAS SCALES ", result.scales.len
   # TODO: fill others with defaults
 
 func geom_point*(): Geom =
@@ -83,21 +132,21 @@ func geom_freqpoly*(color: Color = grey20, # color of the line
   result = Geom(kind: gkFreqPoly,
                 style: some(style))
 
+proc geom_tile*(): Geom =
+  result = Geom(kind: gkTile)
+
 proc ggtitle*(title: string, subtitle = ""): (string, string) = (title, subtitle)
 
 proc createLegend(view: var Viewport,
                   title: string,
                   markers: seq[GraphObject],
-                  cat: OrderedTable[string, Color]) =
+                  cat: Scales) =
   ## creates a full legend within the given viewport based on the categories
   ## in `cat` with a headline `title` showing data points of `markers`
   let startIdx = view.len
-  view.layout(1, rows = cat.len + 1)
+  view.layout(1, rows = cat.colorMap.len + 1)
   # iterate only over added children, skip first, because we actual legend first
   var j = 0
-  var catSeq: seq[string]
-  for k, v in cat:
-    catSeq.add k
   for i in startIdx + 1 ..< view.len:
     # create rectangle showing style of data points
     var ch = view[i]
@@ -116,14 +165,14 @@ proc createLegend(view: var Viewport,
     let point = ch.initPoint(Coord(x: c1(ch.height.val / 2.0 * viewRatio),
                                    y: c1(0.5)),
                              marker = markers[j].ptMarker,
-                             color = cat[catSeq[j]])
+                             color = cat.getColor(cat.getColorKey(j)))
 
     let label = ch.initText(
       Coord(
         x: c1(ch.height.val * viewRatio) +
            c1(quant(0.3, ukCentimeter).toRelative(some(ch.wImg)).val),
         y: c1(0.5)),
-      catSeq[j],
+      cat.getColorKey(j),
       alignKind = taLeft
     )
     ch.addObj [rect, point, label]
@@ -171,7 +220,7 @@ proc `+`*(p: GgPlot, scale: Scales): GgPlot =
 proc `+`*(p: GgPlot, aes: Aesthetics): GgPlot =
   ## adds the given aesthetics to the GgPlot object
   result = p
-  result.aes.add aes
+  result.addAes aes
 
 proc `+`*(p: GgPlot, titleTup: (string, string)): GgPlot =
   ## adds the given title / subtitle to the GgPlot object
@@ -256,6 +305,105 @@ proc dataTo[T: Table | OrderedTable | DataFrame; U](
         raise newException(Exception, "Column " & $col & " has no data!")
       else: discard
 
+proc createPointGobj(view: var Viewport, p: GgPlot, geom: Geom): seq[GraphObject] =
+  ## creates the GraphObjects for a `gkPoint` geom
+  doAssert geom.kind == gkPoint
+  for aes in p.aes:
+    let xData = p.data.dataTo(aes.x, float)
+    let yData = p.data.dataTo(aes.y.get, float)
+    for i in 0 ..< xData.len:
+      if aes.color.isSome:
+        let xx = p.scales[1]
+
+        #let style = Style(fillColor: colorsCat[colors[i]])
+        result.add initPoint(view, (x: xData[i], y: yData[i]),
+                           marker = mkCircle,
+                           color = p.scales[1].getColor(i))
+      else:
+        result.add initPoint(view, (x: xData[i], y: yData[i]),
+                           marker = mkCircle)
+
+proc createHistFreqPolyGobj(view: var Viewport, p: GgPlot, geom: Geom): seq[GraphObject] =
+  for aes in p.aes:
+    # before performing a calculation for the histogram viewports, get the
+    # new xScale, by calling calcTickLocations with it
+    # Note: we don't have to assign it to the `view` viewport, since that will
+    # happen when calculation of the actual ticks will be done later on
+    let (newXScale, _, _) = calcTickLocations(view.xScale, p.scales[0].numXTicks)
+
+    # generate the histogram itself
+    let rawDat = p.data.dataTo(aes.x, float)
+    const nbins = 30
+    let binWidth = (newXScale.high - newXScale.low).float / nbins.float
+
+    # TODO: if aes.colos.isSome we have to group the data we histogram first
+    # and calculate histograms for each
+
+    let hist = rawDat.histogram(bins = nbins, range = (newXScale.low, newXScale.high))
+
+    # given the histogram, we can now deduce the base yScale we need
+    let yScaleBase = (low: 0.0, high: hist.max.float)
+    # however, this is not the final one, since we still have to calculate
+    # the tick locations, which might change it again
+    let (newYScale, _, _) = calcTickLocations(yScaleBase, p.scales[0].numYTicks)
+
+    # create viewports showing the bars
+    view.yScale = newYScale
+
+    var style: Option[Style]
+    if geom.style.isSome:
+      style = geom.style
+    else:
+      # TODO: inherit from parent somehow?
+      discard
+    if geom.kind == gkHistogram:
+      view.layout(nbins, 1)
+      var i = 0
+      for p in mitems(view):
+        doAssert p.yScale.high >= hist.max.float
+        echo "I ", i , " for len ", hist.len
+        let yPos = 1.0 - quant(hist[i].float, ukData).toRelative(scale = some(p.yScale)).val
+        let r = p.initRect(c(0.0, yPos), # bottom left
+                           quant(1.0, ukRelative),
+                           quant(hist[i].float, ukData),#.toRelative(scale = some(p.yScale)),
+                           style = style)
+        p.addObj r
+        inc i
+    else:
+      doAssert geom.kind == gkFreqPoly
+      # only single viewport will be used
+      # calculate bin centers
+      let binCenters = linspace(newXScale.low + binWidth / 2.0, newXScale.high - binWidth / 2.0, nbins)
+      # build data points for polyLine
+      var points = newSeq[Point](nbins)
+      for i in 0 ..< nbins:
+        points[i] = (x: binCenters[i], y: hist[i].float)
+
+      # TODO: rewrite code such that user hands style, but we convert to
+      # `Style` in geom?
+      var style: Option[Style]
+      if geom.style.isSome:
+        style = geom.style
+      else:
+        # TODO: somehow inherit something from GgPlot, maybe via themes?
+        discard
+      # have to update the scale of our viewport! `initPolyLine` depends on it
+      # However: we could change the `ginger` code to accept a seq[Coord] instead and
+      # use the `newXScale, newYScale` as the ukData scale instead!
+      view.xScale = newXScale
+      result.add view.initPolyLine(points, style)
+
+proc createGobjFromGeom(view: var Viewport, p: GgPlot, geom: Geom): seq[GraphObject] =
+  ## performs the required conversion of the data from the data
+  ## frame according to the given `geom`
+  case geom.kind
+  of gkPoint:
+    result = view.createPointGobj(p, geom)
+  of gkHistogram, gkFreqPoly:
+    result = view.createHistFreqPolyGobj(p, geom)
+  else:
+    discard
+
 proc ggsave*(p: GgPlot, fname: string) =
   # check if any aes
   doAssert p.aes.len > 0, "Needs at least one aesthetics!"
@@ -284,15 +432,6 @@ proc ggsave*(p: GgPlot, fname: string) =
       maxY = ydata.max
       yScale = (low: minY, high: maxY)
 
-    if aes.color.isSome:
-      var colorCs: seq[Color]
-      colors = p.data.dataTo(aes.color.get, string)
-      # convert to set to filter duplicates, back to seq and sort
-      let catSeq = colors.toSet.toSeq.sorted
-      colorCs = ggColorHue(catSeq.len)
-      for i, k in catSeq:
-        colorsCat[k] = colorCs[i]
-
   # create the plot
   var img = initViewport(xScale = some(xScale),
                          yScale = some(yScale))
@@ -308,7 +447,6 @@ proc ggsave*(p: GgPlot, fname: string) =
   plt.background()
 
   for geom in p.geoms:
-    var data = newSeq[GraphObject]()
     # for each geom, we create a child viewport of `plt` covering
     # the whole viewport, which will house the data we just created.
     # Due to being a child, if will be drawn *after* its parent. This way things like
@@ -317,103 +455,22 @@ proc ggsave*(p: GgPlot, fname: string) =
     # order of the function calls `geom_*` be preserved
     var pChild = plt.addViewport()
 
-    case geom.kind
-    of gkPoint:
-      for aes in p.aes:
-        let xData = p.data.dataTo(aes.x, float)
-        let yData = p.data.dataTo(aes.y.get, float)
-        for i in 0 ..< xData.len:
-          if aes.color.isSome:
-            #let style = Style(fillColor: colorsCat[colors[i]])
-            data.add initPoint(pChild, (x: xData[i], y: yData[i]),
-                               marker = mkCircle,
-                               color = colorsCat[colors[i]])
-          else:
-            data.add initPoint(pChild, (x: xData[i], y: yData[i]),
-                               marker = mkCircle)
-    of gkHistogram, gkFreqPoly:
-      for aes in p.aes:
-
-        # before performing a calculation for the histogram viewports, get the
-        # new xScale, by calling calcTickLocations with it
-        # Note: we don't have to assign it to the `pChild` viewport, since that will
-        # happen when calculation of the actual ticks will be done later on
-        let (newXScale, _, _) = calcTickLocations(xScale, numXTicks)
-
-        # generate the histogram itself
-        let rawDat = p.data.dataTo(aes.x, float)
-        const nbins = 30
-        let binWidth = (newXScale.high - newXScale.low).float / nbins.float
-
-        # TODO: if aes.colos.isSome we have to group the data we histogram first
-        # and calculate histograms for each
-
-        let hist = rawDat.histogram(bins = nbins, range = (newXScale.low, newXScale.high))
-
-        # given the histogram, we can now deduce the base yScale we need
-        let yScaleBase = (low: 0.0, high: hist.max.float)
-        # however, this is not the final one, since we still have to calculate
-        # the tick locations, which might change it again
-        let (newYScale, _, _) = calcTickLocations(yScaleBase, numYTicks)
-
-        # create viewports showing the bars
-        pChild.yScale = newYScale
-
-        var style: Option[Style]
-        if geom.style.isSome:
-          style = geom.style
-        else:
-          # TODO: inherit from parent somehow?
-          discard
-        if geom.kind == gkHistogram:
-          pChild.layout(nbins, 1)
-          var i = 0
-          for p in mitems(pChild):
-            doAssert p.yScale.high >= hist.max.float
-            echo "I ", i , " for len ", hist.len
-            let yPos = 1.0 - quant(hist[i].float, ukData).toRelative(scale = some(p.yScale)).val
-            let r = p.initRect(c(0.0, yPos), # bottom left
-                               quant(1.0, ukRelative),
-                               quant(hist[i].float, ukData),#.toRelative(scale = some(p.yScale)),
-                               style = style)
-            p.addObj r
-            inc i
-        else:
-          doAssert geom.kind == gkFreqPoly
-          # only single viewport will be used
-          # calculate bin centers
-          let binCenters = linspace(newXScale.low + binWidth / 2.0, newXScale.high - binWidth / 2.0, nbins)
-          # build data points for polyLine
-          var points = newSeq[Point](nbins)
-          for i in 0 ..< nbins:
-            points[i] = (x: binCenters[i], y: hist[i].float)
-
-          # TODO: rewrite code such that user hands style, but we convert to
-          # `Style` in geom?
-          var style: Option[Style]
-          if geom.style.isSome:
-            style = geom.style
-          else:
-            # TODO: somehow inherit something from GgPlot, maybe via themes?
-            discard
-          # have to update the scale of our viewport! `initPolyLine` depends on it
-          # However: we could change the `ginger` code to accept a seq[Coord] instead and
-          # use the `newXScale, newYScale` as the ukData scale instead!
-          pChild.xScale = newXScale
-          data.add pChild.initPolyLine(points, style)
-
-      # for these types there is no y data scale attached to the image so far,
-      # thus we assign `pChild`'s data scale to it
-      # TODO: solve this more elegantly!
-      plt.yScale = pChild.yScale
-      img.yScale = pChild.yScale
-    else:
-      discard
+    let data = pChild.createGobjFromGeom(p, geom)
 
     # add the data to the child
     pChild.addObj data
     # add the data viewport to the plt
     plt.children.add pChild
+
+    # for these types there is no y data scale attached to the image so far,
+    # thus we assign `pChild`'s data scale to it
+    # TODO: solve this more elegantly!
+
+    # potentially the creation of the graph objects have altered the scales
+    # of the viewport. We have to make sure that the parents receive an updated
+    # scale too
+    plt.yScale = pChild.yScale
+    img.yScale = pChild.yScale
 
   let
     xticks = plt.xticks(numXTicks)
@@ -440,7 +497,7 @@ proc ggsave*(p: GgPlot, fname: string) =
     lg.origin.y = lg.origin.y + c1(img[4].height.val / 4.0)
     lg.origin.x = lg.origin.x + img.c1(0.5, akX, ukCentimeter)
     var markers: seq[GraphObject]
-    for k, v in colorsCat:
+    for k, v in p.scales[1].colors:
       markers.add initPoint(plt,
                             (0.0, 0.0), # dummy coordinates
                             marker = mkCircle) # assign same marker as above
@@ -450,7 +507,7 @@ proc ggsave*(p: GgPlot, fname: string) =
     #              cat: Table[string, Color]) =
     lg.createLegend(p.aes[0].color.get,
                     markers,
-                    colorsCat)
+                    p.scales[1])
     img[5] = lg
   if p.title.len > 0:
     var titleView = img[1]
@@ -497,7 +554,7 @@ let df = toDf(mpg)
 df.filter(f{"class" == "suv"}) # comparison via `f{}` macro
   .mutate(ratioHwyToCity ~ hwy / cty # raw untyped template function definition
   ) # <- note that we have to use normal UFCS to hand to `ggplot`!
-  .ggplot(aes(x = "ratioHwyToCity", y = "displ", color = "class")) + 
+  .ggplot(aes(x = "ratioHwyToCity", y = "displ", color = "class")) +
   geom_point() +
   ggsave("scatterFromDf.pdf")
 
