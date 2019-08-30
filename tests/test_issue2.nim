9efc0ccb956502b0f787efd18207bc01dece3112
Vindaar <basti90@gmail.com>
2019-08-30 18:54:06 +0200

    add test case for issue #2

---
+++ tests/test_issue2.nim
@@ -0,0 +1,39 @@
+import ggplotnim, ginger
+import seqmath, sequtils
+import unittest
+
+
+test "Issue2":
+  let xdata = toSeq(0 ..< 2560)
+  let ydata = xdata.mapIt(it.float * it.float)
+
+  let plt = ggplot(seqsToDf({ "x" : xdata,
+                              "y" : ydata }),
+                   aes("x", "y")) +
+    geom_line()
+
+  let view = plt.ggcreate()
+  let xScale = (low: 0.0, high: 3000.0)
+  let yScale = (low: 0.0, high: 7000000.0)
+  for ch in view.children:
+    if ch.name == "plot":
+      check ch.xScale == xScale # tick calculation modifies max
+      check ch.yScale == yScale
+      for el in ch.children:
+        if el.name == "data":
+          # data child must inherit the new scales from its plot parent
+          check el.xScale == xScale
+          check el.yScale == yScale
+          for p in el.objects:
+            # plot scales also must inherit its parent's scale
+            check p.kind == goPolyLine
+            for xy in p.plPos:
+              check xy.x.kind == ukData
+              check xy.x.scale == xScale
+              check xy.y.scale == yScale
+
+  # the scale assigned to the total plot does not have to be the one finally
+  # TODO: decide if we want to make sure to update it regardless
+  #check view.xScale == (low: 0.0, high: 3000.0) # tick calculation modifies max
+  check view.yScale == yScale
+  plt.ggsave("test_issue2.pdf")
