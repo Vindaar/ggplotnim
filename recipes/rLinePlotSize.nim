0b2111eea7125a3993341a0964624ed95f5c8dd4
Vindaar <basti90@gmail.com>
2019-11-05 13:14:19 +0100

    [recipes] change recipes.org intro, note about ntangled files & more

---
+++ recipes/rLinePlotSize.nim
@@ -0,0 +1,10 @@
+import ggplotnim, sequtils, seqmath
+const
+  width = 720
+  height = 480
+let x = linspace(0.0, 30.0, 1000)
+let y = x.mapIt(pow(sin(it), 2.0))
+let df = seqsToDf(x, y)
+ggplot(df, aes("x", "y")) +
+  geom_line() +
+  ggsave("rLinePlotSize.png", width = width, height = height)
