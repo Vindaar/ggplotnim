9df90fe12e01d0a3f93bcfd5759f99df0e746379
Vindaar <basti90@gmail.com>
2020-02-03 15:05:20 +0100

    [recipes] add nim file for bar plot w/ rotated labels recipe

---
+++ recipes/rBarPlotRotatedLabels.nim
@@ -0,0 +1,7 @@
+import ggplotnim
+let df = toDf(readCsv("data/fake_shifter_data.txt"))
+ggplot(df, aes("Shifters", fill = "Year")) +
+  geom_bar() +
+  xlab(rotate = -45.0, margin = 1.75, alignTo = "right") +
+  ggtitle("Number of shifts done by each shifter by year") +
+  ggsave("media/recipes/rBarPlotRotatedLabels.svg", width = 3000, height = 500)
