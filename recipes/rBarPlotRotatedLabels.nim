7a2c4c8c18d13037c7b7b9770fd533d9dde56617
Vindaar <basti90@gmail.com>
2020-02-03 15:25:55 +0100

    [tests] attempt to fix bar plot via png

--- recipes/rBarPlotRotatedLabels.nim
+++ recipes/rBarPlotRotatedLabels.nim
@@ -4,4 +4,4 @@ ggplot(df, aes("Shifters", fill = "Year")) +
   geom_bar() +
   xlab(rotate = -45.0, margin = 1.75, alignTo = "right") +
   ggtitle("Number of shifts done by each shifter by year") +
-  ggsave("media/recipes/rBarPlotRotatedLabels.svg", width = 3000, height = 500)
+  ggsave("media/recipes/rBarPlotRotatedLabels.png", width = 5000, height = 1000)
