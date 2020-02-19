757dcf1a6b47cb90bf4bc8e8b7ecfd580f7d3333
Vindaar <basti90@gmail.com>
2020-02-19 14:54:02 +0100

    add recipe for custom annotations

--- tests/tCompareRecipes.nim
+++ tests/tCompareRecipes.nim
@@ -38,7 +38,8 @@ suite "Compare recipe output":
                   "rStackedMpgFreqpoly.png",
                   "rMpgStackedBarPlot.png",
                   "rBarPlotRotatedLabels.png",
-                  "rBarPlotCompStats.png"]
+                  "rBarPlotCompStats.png",
+                  "rCustomAnnotations.png"]
     proc convertRead(path: string): seq[seq[string]] =
       for i, f in files:
         let pathF = path / $f
