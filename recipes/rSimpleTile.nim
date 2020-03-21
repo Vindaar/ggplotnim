175d2a7eaec3656df0d01c101b40eb96fa8db6fb
Vindaar <basti90@gmail.com>
2020-03-21 15:32:49 +0100

    add recipes as nim file

---
+++ recipes/rSimpleTile.nim
@@ -0,0 +1,16 @@
+import ggplotnim, random
+var
+  xs = newSeq[float]()
+  ys = newSeq[float]()
+  zs = newSeq[float]()
+for x in 0 ..< 28:
+  for y in 0 ..< 28:
+    xs.add x.float
+    ys.add y.float
+    zs.add random(1.0)
+let df = seqsToDf(xs, ys, zs)
+ggplot(df, aes("xs", "ys", fill = "zs")) +
+  geom_tile() +
+  #scale_x_continuous(dcKind = dcDiscrete) +
+  #scale_y_continuous(dcKind = dcDiscrete) +
+  ggsave("media/recipes/rSimpleTile.png")
