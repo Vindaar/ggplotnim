b08ba5b5b1bac64dd53b8cf5fd15d9e8b2c3c6b8
Vindaar <basti90@gmail.com>
2020-03-26 00:22:10 +0100

    add more recipes about geom_text + tile, subplots

---
+++ recipes/rAnnotatedHeatmap.nim
@@ -0,0 +1,11 @@
+import ggplotnim, random, math
+
+let df = toDf(readCsv("data/mpg.csv"))
+let dfRed = df.group_by(["class", "cyl"]).summarize(f{mean("hwy")})
+# stringification of formula is default name
+let meanHwyCol = $f{mean("hwy")}
+ggplot(dfRed, aes("class", "cyl", fill = meanHwyCol)) +
+  geom_tile() +
+  geom_text(aes(text = meanHwyCol)) +
+  scale_y_continuous(dcKind = dcDiscrete) +
+  ggsave("media/recipes/rAnnotatedHeatmap.png")
