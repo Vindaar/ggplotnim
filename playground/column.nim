380ffad4c7b2303a9e216cf5080949d38007f26b
Vindaar <basti90@gmail.com>
2020-03-28 16:19:43 +0100

    arraymancer backend w/ feature parity in playground!
    Still very dirty and slight API changes of formula, but that can
    either be fixed / is an improvement.

---
+++ playground/column.nim
@@ -0,0 +1,385 @@
+import arraymancer, value, sugar
+
+type
+  ColKind* = enum
+    colFloat, colInt, colBool, colString, colObject
+  Column* = object
+    len*: int
+    case kind*: ColKind
+    of colFloat: fCol*: Tensor[float]
+    of colInt: iCol*: Tensor[int]
+    of colBool: bCol*: Tensor[bool]
+    of colString: sCol*: Tensor[string]
+    of colObject: oCol*: Tensor[Value]
+
+func toColumn*[T: float | int | string | bool | Value](t: Tensor[T]): Column =
+  when T is int:
+    result = Column(kind: colInt,
+                    iCol: t,
+                    len: t.size)
+  elif T is float:
+    result = Column(kind: colFloat,
+                    fCol: t,
+                    len: t.size)
+  elif T is bool:
+    result = Column(kind: colBool,
+                    bCol: t,
+                    len: t.size)
+  elif T is string:
+    result = Column(kind: colString,
+                    sCol: t,
+                    len: t.size)
+  elif T is Value:
+    result = Column(kind: colObject,
+                    oCol: t,
+                    len: t.size)
+
+proc `[]`*(c: Column, slice: Slice[int]): Column =
+  case c.kind
+  of colInt: result = toColumn c.iCol[slice.a .. slice.b]
+  of colFloat: result = toColumn c.fCol[slice.a .. slice.b]
+  of colString: result = toColumn c.sCol[slice.a .. slice.b]
+  of colBool: result = toColumn c.bCol[slice.a .. slice.b]
+  of colObject: result = toColumn c.oCol[slice.a .. slice.b]
+
+func initColumn*(kind: ColKind, length = 0): Column =
+  case kind
+  of colFloat: result = toColumn newTensor[float](length)
+  of colInt: result = toColumn newTensor[int](length)
+  of colString: result = toColumn newTensor[string](length)
+  of colBool: result = toColumn newTensor[bool](length)
+  of colObject: result = toColumn newTensor[Value](length)
+
+func toColKind*[T](dtype: typedesc[T]): ColKind =
+  when T is float:
+    result = colFloat
+  elif T is int:
+    result = colInt
+  elif T is bool:
+    result = colBool
+  elif T is string:
+    result = colString
+  elif T is Value:
+    result = colObject
+
+func toColKind*[T](vKind: ValueKind): ColKind =
+  case vKind
+  of VFloat: result = colFloat
+  of VInt: result = colInt
+  of VString: result = colString
+  of VBool: result = colBool
+  of VObject: result = colObject
+
+proc asValue*[T](t: Tensor[T]): Tensor[Value] {.noInit.} =
+  ## Apply type conversion on the whole tensor
+  result = t.map(x => (%~ x))
+
+proc valueTo*[T](t: Tensor[Value], dtype: typedesc[T]): Tensor[T] =
+  when T is string:
+    result = t.map(x => x.toStr)
+  elif T is float:
+    result = t.map(x => x.toFloat)
+  elif T is int:
+    result = t.map(x => x.toInt)
+  elif T is bool:
+    result = t.map(x => x.toBool)
+  elif T is Value:
+    result = t
+
+func toTensor*[T](c: Column, dtype: typedesc[T]): Tensor[T] =
+  case c.kind
+  of colInt:
+    when T is int:
+      result = c.iCol
+    elif T is SomeNumber:
+      result = c.iCol.asType(T)
+    elif T is Value:
+      result = c.iCol.asValue
+  of colFloat:
+    when T is float:
+      result = c.fCol
+    elif T is SomeNumber:
+      result = c.fCol.asType(T)
+    elif T is Value:
+      result = c.fCol.asValue
+  of colString:
+    when T is string:
+      result = c.sCol
+    elif T is Value:
+      result = c.sCol.asValue
+  of colBool:
+    when T is bool:
+      result = c.bCol
+    elif T is Value:
+      result = c.bCol.asValue
+  of colObject:
+    result = c.oCol.valueTo(T)
+
+func toTensor*[T](c: Column, slice: Slice[int], dtype: typedesc[T]): Tensor[T] =
+  case c.kind
+  of colInt:
+    when T is int:
+      result = c.iCol[slice.a .. slice.b]
+    elif T is SomeNumber:
+      result = c.iCol[slice.a .. slice.b].asType(T)
+  of colFloat:
+    when T is float:
+      result = c.fCol[slice.a .. slice.b]
+    elif T is SomeNumber:
+      result = c.fCol[slice.a .. slice.b].asType(T)
+  of colString:
+    when T is string:
+      result = c.sCol[slice.a .. slice.b]
+  of colBool:
+    when T is bool:
+      result = c.bCol[slice.a .. slice.b]
+  of colObject:
+    result = c.oCol[slice.a .. slice.b].valueTo(T)
+
+proc `[]`*[T](c: Column, idx: int, dtype: typedesc[T]): T =
+  when T isnot Value:
+    case c.kind
+    of colInt:
+      when T is int:
+        result = c.iCol[idx]
+      elif T is SomeNumber:
+        result = c.iCol[idx].T
+    of colFloat:
+      when T is float:
+        result = c.fCol[idx]
+      elif T is SomeNumber:
+        result = c.fCol[idx].T
+    of colString:
+      when T is string:
+        result = c.sCol[idx]
+    of colBool:
+      when T is bool:
+        result = c.bCol[idx]
+    of colObject:
+      when T is string:
+        result = c.oCol[idx].toStr
+      elif T is float:
+        result = c.oCol[idx].toFloat
+      elif T is int:
+        result = c.oCol[idx].toInt
+      elif T is bool:
+        result = c.oCol[idx].toBool
+  else:
+    case c.kind
+    of colInt: result = %~ c.iCol[idx]
+    of colFloat: result = %~ c.fCol[idx]
+    of colString: result = %~ c.sCol[idx]
+    of colBool: result = %~ c.bCol[idx]
+    of colObject: result = c.oCol[idx]
+
+proc `[]=`*[T](c: var Column, idx: int, val: T) =
+  ## assign `val` to column `c` at index `idx`
+  ## If the types match, it just calls `[]=` on the tensor.
+  ## If they are compatible, `val` is converted to c's type.
+  ## If they are incompatible, `c` will be rewritten to an object
+  ## column.
+  var rewriteAsValue = false
+  case c.kind
+  of colFloat:
+    when T is float:
+      c.fCol[idx] = val
+    elif T is SomeNumber:
+      c.fCol[idx] = val.float
+  of colInt:
+    when T is int:
+      c.iCol[idx] = val
+    else:
+      rewriteAsValue = true
+  of colString:
+    when T is string:
+      c.sCol[idx] = val
+    else:
+      rewriteAsValue = true
+  of colBool:
+    when T is bool:
+      c.bCol[idx] = val
+    else:
+      rewriteAsValue = true
+  of colObject:
+    c.oCol[idx] = %~ val
+  if rewriteAsValue:
+    # rewrite as an object column
+    c = c.toObjectColumn()
+    c.oCol[idx] = %~ val
+
+template withNativeTensor*(c: Column,
+                           valName: untyped,
+                           body: untyped): untyped =
+  case c.kind
+  of colInt:
+    let `valName` {.inject.} =  c.iCol
+    body
+  of colFloat:
+    let `valName` {.inject.} =  c.fCol
+    body
+  of colString:
+    let `valName` {.inject.} =  c.sCol
+    body
+  of colBool:
+    let `valName` {.inject.} =  c.bCol
+    body
+  of colObject:
+    let `valName` {.inject.} =  c.oCol
+    body
+
+template `%~`*(v: Value): Value = v
+
+func toObjectColumn*(c: Column): Column =
+  ## returns `c` as an object column
+  var res = newTensor[Value](c.len)
+  withNativeTensor(c, t):
+    for idx in 0 ..< c.len:
+      res[idx] = %~ (t[idx])
+
+template withNative*(c: Column, idx: int,
+                     valName: untyped,
+                     body: untyped): untyped =
+  case c.kind
+  of colInt:
+    let `valName` {.inject.} =  c[idx, int]
+    body
+  of colFloat:
+    let `valName` {.inject.} =  c[idx, float]
+    body
+  of colString:
+    let `valName` {.inject.} =  c[idx, string]
+    body
+  of colBool:
+    let `valName` {.inject.} =  c[idx, bool]
+    body
+  of colObject:
+    let `valName` {.inject.} =  c[idx, Value]
+    body
+
+template withNativeDtype*(c: Column, body: untyped): untyped =
+  case c.kind
+  of colInt:
+    type dtype {.inject.} = int
+    body
+  of colFloat:
+    type dtype {.inject.} = float
+    body
+  of colString:
+    type dtype {.inject.} = string
+    body
+  of colBool:
+    type dtype {.inject.} = bool
+    body
+  of colObject:
+    type dtype {.inject.} = Value
+    body
+
+proc equal*(c1: Column, idx1: int, c2: Column, idx2: int): bool =
+  ## checks if the value in `c1` at `idx1` is equal to the
+  ## value in `c2` at `idx2`
+  if c1.kind != c2.kind: return false
+  withNativeDtype(c1):
+    result = c1[idx1, dtype] == c2[idx2, dtype]
+
+template withNative2*(c1, c2: Column, idx1, idx2: int,
+                      valName1, valName2: untyped,
+                      body: untyped): untyped =
+  assert c1.kind == c2.kind
+  case c1.kind
+  of colInt:
+    let `valName1` {.inject.} =  c1[idx1, int]
+    let `valName2` {.inject.} =  c2[idx2, int]
+    body
+  of colFloat:
+    let `valName1` {.inject.} =  c1[idx1, float]
+    let `valName2` {.inject.} =  c2[idx2, float]
+    body
+  of colString:
+    let `valName1` {.inject.} =  c1[idx1, string]
+    let `valName2` {.inject.} =  c2[idx2, string]
+    body
+  of colBool:
+    let `valName1` {.inject.} =  c1[idx1, bool]
+    let `valName2` {.inject.} =  c2[idx2, bool]
+    body
+  of colObject:
+    let `valName1` {.inject.} =  c1[idx1, Value]
+    let `valName2` {.inject.} =  c2[idx2, Value]
+    body
+
+func compatibleColumns*(c1, c2: Column): bool {.inline.} =
+  if c1.kind == c2.kind: result = true
+  elif c1.kind in {colInt, colFloat} and
+       c2.kind in {colInt, colFloat}:
+    result = true
+  else: result = false
+
+func toObject*(c: Column): Column {.inline.} =
+  case c.kind
+  of colObject: result = c
+  of colInt: result = toColumn c.iCol.asValue
+  of colFloat: result = toColumn c.fCol.asValue
+  of colString: result = toColumn c.sCol.asValue
+  of colBool: result = toColumn c.bCol.asValue
+
+func add*(c1, c2: Column): Column =
+  ## adds column `c2` to `c1`. Uses `concat` internally.
+  if c2.len == 0: return c1
+  elif c1.len == 0: return c2
+  if c1.kind == c2.kind:
+    # just concat directly
+    case c1.kind
+    of colInt: result = toColumn concat(c1.iCol, c2.iCol, axis = 0)
+    of colFloat: result = toColumn concat(c1.fCol, c2.fCol, axis = 0)
+    of colBool: result = toColumn concat(c1.bCol, c2.bCol, axis = 0)
+    of colString: result = toColumn concat(c1.sCol, c2.sCol, axis = 0)
+    of colObject: result = toColumn concat(c1.oCol, c2.oCol, axis = 0)
+  elif compatibleColumns(c1, c2):
+    # convert both to float
+    case c1.kind
+    of colInt:
+      # c1 is int, c2 is float
+      assert c2.kind == colFloat
+      result = toColumn concat(c1.fCol.asType(float), c2.fCol, axis = 0)
+    of colFloat:
+      # c1 is float, c2 is int
+      assert c2.kind == colInt
+      result = toColumn concat(c1.fCol, c2.iCol.asType(float), axis = 0)
+    else: doAssert false, "cannot happen, since not compatible!"
+  else:
+    # convert both columns to Value
+    result = toColumn concat(c1.toObject.oCol, c2.toObject.oCol, axis = 0)
+  result.len = c1.len + c2.len
+
+proc toColumn*[T: float | string | int | bool | Value](s: openArray[T]): Column =
+  var vals = newTensor[T](s.len)
+  for i, x in s:
+    vals[i] = x
+  result = toColumn(vals)
+
+proc toColumn*[T: float | string | int | bool | Value](x: T): Column =
+  # also possible to create single row column, but inefficient
+  # for `summarize` though there's no way around
+  let vals = newTensorWith[T](1, x)
+  result = toColumn(vals)
+
+proc toNativeColumn*(s: openArray[Value]): Column =
+  ## given input as `Value`, will attempt to return the column as native
+  ## data type.
+  ## NOTE: this is unsafe and assumes the values are indeed all one type!
+  if s.len > 0:
+    withNativeConversion(s[0].kind, get):
+      var data = newTensor[dtype](s.len)
+      for i, x in s:
+        data[i] = get(x)
+      result = toColumn data
+
+func nullColumn*(num: int): Column =
+  ## returns an object `Column` with `N` values, which are
+  ## all `VNull`
+  var nullseq = newSeq[Value](num)
+  for i in 0 ..< num:
+    nullseq[i] = Value(kind: VNull)
+  result = toColumn(nullseq)
+
+#proc `*`[T: SomeNumber]*(c: Column, x: T)
---
+++ playground/value.nim
@@ -0,0 +1,498 @@
+import tables, strutils, math, fenv, parseutils, strformat, hashes
+
+type
+  ValueKind* = enum
+    VNull,
+    VBool,
+    VInt,
+    VFloat,
+    VString,
+    VObject
+
+  Value* = object
+    case kind*: ValueKind
+    of VString:
+      str*: string
+    of VInt:
+      num*: int #BiggestInt
+    of VFloat:
+      fnum*: float
+    of VBool:
+      bval*: bool
+    of VObject:
+      fields*: OrderedTable[string, Value] # alternative: `seq[(string, Value)]` pairs?
+    of VNull:
+      discard
+
+func toValKind*[T](dtype: typedesc[T]): ValueKind =
+  when T is float:
+    result = VFloat
+  elif T is int:
+    result = VInt
+  elif T is bool:
+    result = VBool
+  elif T is string:
+    result = VString
+  elif T is Value:
+    result = VObject
+
+iterator items*(row: Value): Value =
+  doAssert row.kind == VObject
+  for v in values(row.fields):
+    yield v
+
+iterator keys*(row: Value): string =
+  doAssert row.kind == VObject
+  for k in keys(row.fields):
+    yield k
+
+iterator pairs*(row: Value): tuple[key: string, val: Value] =
+  ## Iterator for the elements of `row`. `row` has to be a JObject
+  ## representing a row of a `DataFrame`
+  assert row.kind == VObject
+  for key, val in pairs(row.fields):
+    yield (key, val)
+
+proc contains*(v: Value, key: string): bool =
+  doAssert v.kind == VObject
+  result = v.fields.hasKey(key)
+
+proc `%~`*(c: char): Value =
+  ## we convert a `char` to a `string`!
+  result = Value(kind: VString, str: $c)
+
+proc `%~`*(v: string): Value =
+  result = Value(kind: VString, str: v)
+
+proc `%~`*(v: SomeFloat): Value =
+  result = Value(kind: VFloat, fnum: v.float)
+
+proc `%~`*(v: SomeInteger): Value =
+  result = Value(kind: VInt, num: v.int)
+
+proc `%~`*(v: bool): Value =
+  result = Value(kind: VBool, bval: v)
+
+#proc `%~`*(v: Table[string, Value]): Value =
+#  result = Value(kind: VObject, fields: v.toOrderedTable)
+
+proc `%~`*(v: OrderedTable[string, Value]): Value =
+  result = Value(kind: VObject, fields: v)
+
+proc newVObject*(length = 8): Value =
+  result = Value(kind: VObject)
+  result.fields = initOrderedTable[string, Value](length)
+
+proc `%~`*[T: not Value](s: openArray[T]): seq[Value] =
+  ## converts a `seq[T]` to a `seq[Value]`
+  result = newSeq[Value](s.len)
+  for i, x in s:
+    result[i] = %~ x
+
+template `%~`*(s: openArray[Value]): seq[Value] = @s
+
+proc toObject*(s: seq[(string, Value)]): Value =
+  ## converts the given sequence to an object
+  ## This is only used to store the result of the `groups` iterator as a
+  ## `Value`.
+  result = Value(kind: VObject)
+  result.fields = initOrderedTable[string, Value]()
+  for (key, val) in s:
+    result.fields[key] = val
+
+proc `[]`*(v: Value, key: string): Value {.inline.} =
+  doAssert v.kind == VObject
+  result = v.fields[key]
+
+proc `[]=`*(v: var Value, key: string, val: Value) {.inline.} =
+  doAssert v.kind == VObject
+  v.fields[key] = val
+
+func isNumber*(s: string): bool =
+  ## returns true, if `s` is a number according to our rules:
+  ## - starts with {0..9}
+  ## - ends with {0..9}
+  ## - may contain a single `.`
+  ## - may contain a single `e`, `E`
+  ## - may contain one minus, one plus at beginning and one for exponent
+  ## - else may only contain {0..9}
+  ## - `e`, `+`, `-`, `.` may not appear one after another
+  ## - may contain space before and after the number
+  ## It is only used to decide whether the stringifaction of `s`
+  ## will be surrounded by `"`.
+  var idx = skipWhile(s, toSkip = {' '})
+  template next(checkFor: untyped): untyped =
+    if idx < s.len - 1:
+      s[idx + 1] in checkFor
+    else:
+      false
+  var
+    negMinus = false
+    posPlus = false
+    expMinus = false
+    expPlus = false
+    numBeforeDot = false
+    numBeforeExp = false
+    dot = false
+    expE = false
+    sinceLastSpace = -1
+  while idx < s.len:
+    case s[idx]
+    of '-':
+      if next({'-', '+', '.'}):
+        # another `-+.` after `-`
+        return false
+      elif not negMinus:
+        negMinus = true
+      elif not expMinus:
+        expMinus = true
+      else:
+        # apparently has 3 minus
+        return false
+    of '+':
+      if next({'+', '-'}):
+        # another `-+.` after `-`
+        return false
+      elif not posPlus:
+        posPlus = true
+      elif not expPlus:
+        posPlus = true
+      else:
+        # apparently has 3 plus
+        return false
+    of '0' .. '9':
+      if not dot:
+        numBeforeDot = true
+      if not expE:
+        numBeforeExp = true
+      inc idx
+      continue
+    of '.':
+      if next({'e', 'E'}):
+        # `e` after `.`
+        return false
+      elif not dot and numBeforeDot:
+        dot = true
+      else:
+        # multiple dots or number before `dot`
+        return false
+    of 'e', 'E':
+      if not next({'0'..'9', '-', '+'}):
+        # apparently ends with an 'e', 'E'
+        return false
+      if not expE and numBeforeExp:
+        expE = true
+      else:
+        # multiple `e` or no number before `e`
+        return false
+    of ' ':
+      if sinceLastSpace == -1 or sinceLastSpace == 1:
+        # when we encounter a space, set our `spaceCounter` to 0 to start
+        # increasing it every itereation in main loop
+        sinceLastSpace = 0
+      elif sinceLastSpace > 1:
+        # apparently something between last space and this space
+        return false
+    else: return false # something not part of a number
+    inc idx
+    if sinceLastSpace >= 0:
+      # last iter found a space, so count spaces
+      inc sinceLastSpace
+  return true
+
+func isNumber*(v: Value): bool =
+  doAssert v.kind == VString
+  result = v.str.isNumber
+
+func isInt*(s: string): bool =
+  ## simple "most likely int" check. If the string only contains digits and
+  ## `_` we consider it an Int
+  s.allCharsInSet({'0' .. '9', '_'})
+
+func isInt*(v: Value): bool =
+  ## checks whether the string contained in `Value` is likely an integer
+  ## For an `isFloat` equivalent see `isNumber`.
+  doAssert v.kind == VString
+  result = v.str.isInt
+
+proc toFloat*(v: Value, allowNull: static bool = false): float =
+  when not allowNull:
+    doAssert v.kind in {VInt, VFloat}
+  else:
+    doAssert v.kind in {VInt, VFloat, VNull}
+  case v.kind
+  of VInt: result = v.num.float
+  of VFloat: result = v.fnum
+  of VNull:
+    # This branch is forbidden for `allowNull = false` due to `doAssert` at top!
+    result = 0.0
+  else: discard
+
+proc toInt*(v: Value): int = #BiggestInt =
+  ## Converts a numeric value to an int. If the value is a float
+  ## we round and convert to int
+  doAssert v.kind in {VInt, VFloat}
+  case v.kind
+  of VInt: result = v.num
+  of VFloat: result = v.fnum.round.int
+  else: discard
+
+proc toBool*(v: Value): bool =
+  ## Checks if the value is a bool and returns its value
+  doAssert v.kind == VBool
+  result = v.bval
+
+proc toStr*(v: Value): string =
+  ## Returns the value `v` as a string. If the value is of kind `VString`,
+  ## no conversion is required.
+  ## This however will fail, if the input is of type
+  ## - VNull
+  ## - VObject
+  ## if you want string representations of those value types, use `$`
+  case v.kind
+  of VInt, VFloat, VBool: result = $v
+  of VString: result = v.str
+  else:
+    raise newException(ValueError, "Will not convert a Value of kind " &
+      $v.kind & " to string! Use `$` for that!")
+
+template withNative*(v: Value,
+                     valName: untyped,
+                     body: untyped): untyped =
+  case v.kind
+  of VInt:
+    let `valName` {.inject.} =  c.num
+    body
+  of VFloat:
+    let `valName` {.inject.} =  c.fnum
+    body
+  of VString:
+    let `valName` {.inject.} =  c.str
+    body
+  of VBool:
+    let `valName` {.inject.} =  c.bval
+    body
+  of VObject:
+    doAssert false, "not implemented / makes no sense for current usage"
+
+template withNativeConversion*(kind: ValueKind,
+                               procName: untyped,
+                               body: untyped): untyped =
+  ## generates an environment, in which the correct `to*` proc
+  ## is named `procName` for `kind`
+  case kind
+  of VInt:
+    template `procName`(v: Value): untyped = v.toInt
+    type dtype {.inject.} = int
+    body
+  of VFloat:
+    template `procName`(v: Value): untyped = v.toFloat
+    type dtype {.inject.} = float
+    body
+  of VString:
+    template `procName`(v: Value): untyped = v.toStr
+    type dtype {.inject.} = string
+    body
+  of VBool:
+    template `procName`(v: Value): untyped = v.toBool
+    type dtype {.inject.} = bool
+    body
+  of VObject, VNull:
+    doAssert false, "not implemented / makes no sense for current usage"
+
+
+func isNull*(v: Value): Value =
+  ## returns whether `v` is a `VNull` value as a `VBool`
+  result = %~ (v.kind == VNull)
+
+func almostEqual*(a, b: float, epsilon = 1e-8): bool =
+  # taken from
+  # https://floating-point-gui.de/errors/comparison/
+  let
+    absA = abs(a)
+    absB = abs(b)
+    diff = abs(a - b)
+  if a == b: # shortcut, handles infinities
+    result = true
+  elif a == 0 or b == 0 or (absA + absB) < minimumPositiveValue(float64):
+    # a or b is zero or both are extremely close to it
+    # relative error is less meaningful here
+    result = diff < (epsilon * minimumPositiveValue(float64))
+  else:
+    # use relative error
+    result = diff / min(absA + absB, maximumPositiveValue(float64)) < epsilon
+
+proc `==`*(v, w: Value): bool =
+  ## checks whether the values are equal.
+  ## Note: if both values are numbers of different kind (`VInt` and `VFloat`) the
+  ## values are both compared as floats!
+  ## The float comparison happens with a floating point comparison with relatively
+  ## large epsilon (1-e8).
+  if v.kind != w.kind and
+     v.kind in {VInt, VFloat} and
+     w.kind in {VInt, VFloat}:
+    result = almostEqual(v.toFloat, w.toFloat)
+  elif v.kind != w.kind:
+    result = false
+  else:
+    case v.kind
+    of VString:
+      result = v.str == w.str
+    of VInt:
+      result = v.num == w.num
+    of VFloat:
+      result = almostEqual(v.fnum, w.fnum)
+    of VBool:
+      result = v.bval == w.bval
+    of VObject:
+      # NOTE: taken from json module
+      # we cannot use OrderedTable's equality here as
+      # the order does not matter for equality here.
+      if v.fields.len != w.fields.len: return false
+      for key, val in v.fields:
+        if not w.fields.hasKey(key): return false
+        if w.fields[key] != val: return false
+      result = true
+    of VNull:
+      result = true
+
+proc `<`*(v, w: Value): bool =
+  ## checks whether the `v` is smaller than `w`
+  ## Note: this is only defined for a subset of the possible types!
+  ## Note2: if both are numbers of different kind (`VInt` and `VFloat`) the
+  ## values are compared as a float! For very large values this would be problematic,
+  ## but here we are lenient and assume the user uses `Value` for small calculations!
+  if v.kind != w.kind and
+     v.kind in {VFloat, VInt} and
+     w.kind in {VFloat, VInt}:
+    result = v.toFloat < w.toFloat
+  else:
+    case v.kind
+    of VString:
+      result = v.str < w.str
+    of VInt:
+      result = v.num < w.num
+    of VFloat:
+      result = v.fnum < w.fnum
+    of VBool:
+      result = v.bval < v.bval
+    of VObject:
+      # checks if objects have the same field, and if so whether the
+      # fields of `v` are smaller than those of `w`
+      result = true
+      for k in keys(v):
+        if k notin w:
+          return false
+        if v[k] < w[k]:
+          return true
+        elif v[k] > w[k]:
+          return false
+        # else v[k] is equal to w[k], continue
+    else:
+      raise newException(Exception, "Comparison `<` does not make sense for " &
+        "Value kind " & $v.kind & "!")
+
+proc `<=`*(v, w: Value): bool =
+  ## checks whether `v` is smaller or equal than `w`
+  if v == w:
+    result = true
+  elif v < w:
+    result = true
+
+proc smallerOrFalse*(v: Value, f: float): bool {.inline.} =
+  ## extension of `<` for `Value` to return `false` if `v` is
+  ## not a valid VInt/VFloat.
+  case v.kind
+  of VInt, VFloat: result = v.toFloat < f
+  else: result = false
+
+proc largerOrFalse*(v: Value, f: float): bool {.inline.} =
+  ## extension of `<` for `Value` to return `false` if `v` is
+  ## not a valid VInt/VFloat.
+  case v.kind
+  of VInt, VFloat: result = v.toFloat > f
+  else: result = false
+
+template makeMath(op: untyped): untyped =
+  proc `op`*(v, w: Value): Value =
+    ## Adds two Values together, if they are addeable.
+    ## These operations only work for `VInt` and `VFloat`. `VInt` is converted
+    ## to floats for the calculation. The result is always a `VFloat`!
+    if v.kind in {VFloat, VInt} and
+       w.kind in {VFloat, VInt}:
+      result = Value(kind: VFloat, fnum: `op`(v.toFloat, w.toFloat))
+    elif v.kind == VNull or w.kind == VNull:
+      result = Value(kind: VNull)
+    else:
+      raise newException(Exception, "Math operation does not make sense for " &
+        "Value kind " & $v.kind & "!")
+
+makeMath(`+`)
+makeMath(`-`)
+makeMath(`*`)
+makeMath(`/`)
+
+proc formatFloatValue(v: Value, precision: int): string =
+  ## Performs the formatting of a value of kind `VFloat` to string.
+  ## If the values are smaller < 1e-5 or > 1e5 scientific notation is
+  ## used.
+  doAssert v.kind == VFloat
+  let f = v.fnum
+  if almostEqual(abs(f), 0.0):
+    # to make sure zero is not formatted in scientific
+    result = f.formatBiggestFloat(format = ffDefault,
+                                  precision = precision)
+  elif abs(f) >= 1e5 or abs(f) <= 1e-5:
+    result = f.formatBiggestFloat(format = ffScientific,
+                                  precision = precision)
+  else:
+    result = f.formatBiggestFloat(format = ffDefault,
+                                  precision = precision)
+  result.trimZeros()
+
+proc pretty*(v: Value, precision = 4, emphStrNumber = true): string =
+  ## converts the given value to its value as a string. For `VFloat` the
+  ## precision can be given.
+  ## If `emphStrNumber` is true, a number stored as a string will be emphasized
+  ## by enclosing it with explicit `"`. This is mainly for printing DFs to show
+  ## the user if a number is a number or a string.
+  case v.kind
+  of VInt:
+    result = $v.num
+  of VFloat:
+    result = formatFloatValue(v, precision = precision)
+  of VBool:
+    result = $v.bval
+  of VString:
+    let vstr = v.str
+    if emphStrNumber and (vstr.len == 0 or vstr.isNumber):
+      result = "\"" & vstr & "\""
+    else:
+      result = vstr
+  of VObject:
+    result.add "{"
+    for k, x in v.fields:
+      result.add (&"{k} : {pretty(x)}")
+    result.add "}"
+  of VNull:
+    result = "null"
+
+template `$`*(v: Value): string = pretty(v)
+
+proc hash*(x: Value): Hash =
+  case x.kind
+  of VInt:
+    result = hash(x.num)
+  of VFloat:
+    result = hash(x.fnum)
+  of VString:
+    result = hash(x.str)
+  of VBool:
+    result = hash(x.bval)
+  of VObject:
+    for k, v in x.fields:
+      result = result !& hash(k)
+      result = result !& hash(v)
+  of VNull:
+    result = 0
+  result = !$result
