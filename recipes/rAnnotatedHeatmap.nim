2945022e850760e59e6f4ac959dd504f31f2ae47
Vindaar <basti90@gmail.com>
2020-03-30 17:43:29 +0200

    fix a few more recipes

--- recipes/rAnnotatedHeatmap.nim
+++ recipes/rAnnotatedHeatmap.nim
@@ -1,9 +1,9 @@
 import ggplotnim, random, math
 
 let df = toDf(readCsv("data/mpg.csv"))
-let dfRed = df.group_by(["class", "cyl"]).summarize(f{mean( c"hwy" )})
+let dfRed = df.group_by(["class", "cyl"]).summarize(f{float: "meanHwy" << mean( c"hwy" )})
 # stringification of formula is default name
-let meanHwyCol = $f{mean( c"hwy" )}
+let meanHwyCol = "meanHwy"
 ggplot(dfRed, aes("class", "cyl", fill = meanHwyCol)) +
   geom_tile() +
   geom_text(aes(text = meanHwyCol)) +
