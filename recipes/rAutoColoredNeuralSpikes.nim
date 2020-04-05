7e21df616bc57fe26f55871b3d9e8013f1e75ef0
Vindaar <basti90@gmail.com>
2020-04-05 12:59:26 +0200

    add recipes about neural spike plots

---
+++ recipes/rAutoColoredNeuralSpikes.nim
@@ -0,0 +1,25 @@
+# first start with auto selection of colors
+import ggplotnim, sequtils
+const numx = 50
+const numy = 8
+const lineSizes = [0.4, 0.3, 0.2, 0.8, 0.5, 0.6, 0.7, 0.9]
+# NOTE: The creation of the data here could surely be done in a nicer
+# way...
+var spikes = newSeq[float]()
+var sizes = newSeq[float]()
+for y in 0 ..< numy:
+  for x in 0 ..< numx:
+    spikes.add y.float
+    sizes.add lineSizes[y]
+var df = newDataFrame()
+df["spikes"] = toColumn spikes
+df["neurons"] = toColumn randomTensor(numx * numy, 1.0)
+df["lineSize"] = toColumn sizes
+
+ggplot(df, aes("neurons", "spikes", color = "lineSize")) +
+  geom_linerange(aes(ymin = f{c"spikes" - c"lineSize" / 2.0},
+                     ymax = f{c"spikes" + c"lineSize" / 2.0})) +
+  scale_y_continuous() + # make sure y is considered cont.
+  ylim(-1, 8) + # at the moment ymin, ymax are not considered for the plot range (that's a bug)
+  ggtitle("Spike raster plot") +
+  ggsave("media/recipes/rAutoColoredNeuralSpikes.png")
---
+++ recipes/rCustomColoredNeuralSpikes.nim
@@ -0,0 +1,31 @@
+import ggplotnim, sequtils
+const numx = 50
+const numy = 8
+const lineSizes = [0.4, 0.3, 0.2, 0.8, 0.5, 0.6, 0.7, 0.9]
+# alternatively using fixed colors and one geom_linerange for each color
+let colorCodes = @[color(0, 0, 0),
+                   color(1, 0, 0),
+                   color(0, 1, 0),
+                   color(0, 0, 1),
+                   color(1 , 1, 0),
+                   color(1, 0, 1),
+                   color(0, 1, 1),
+                   color(1, 0, 1)]
+var df = newDataFrame()
+for nr in 0 ..< numy:
+  df["neuron " & $nr] = toColumn randomTensor(numx, 1.0)
+var plt = ggplot(df)
+for nr in 0 ..< numy:
+  # could combine with above loop, but for clarity
+  plt = plt + geom_linerange(aes(x = ("neuron " & $nr),
+                                 y = nr,
+                                 ymin = nr.float - lineSizes[nr] / 2.0,
+                                 ymax = nr.float + lineSizes[nr] / 2.0),
+                             color = some(colorCodes[nr]))
+# finally add scales, title and plot
+plt + scale_y_continuous() + # make sure y is considered cont.
+  ylim(-1, 8) + # at the moment ymin, ymax are not considered for the plot range (that's a bug)
+  xlab("Neurons") +
+  ylab("Spikes") +
+  ggtitle("Spike raster plot, manual colors") +
+  ggsave("media/recipes/rCustomColoredNeuralSpikes.png")
