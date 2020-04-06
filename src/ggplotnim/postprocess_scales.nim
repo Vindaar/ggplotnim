0bafa1007eff02e954e6adb40ac87a0c4fd6446a
Vindaar <basti90@gmail.com>
2020-04-06 12:19:49 +0200

    [scales] fix detection of empty (0, 0) scale

--- src/ggplotnim/postprocess_scales.nim
+++ src/ggplotnim/postprocess_scales.nim
@@ -57,7 +57,7 @@ func mergeScales(s1, s2: ginger.Scale): ginger.Scale =
   ## input data has N elements all 0. This is useful for e.g.
   ## having 0 data, but assigning only a yMin = -1. That results in a
   ## useable (-1, 0) data scale! Currently would not work!
-  if s1.low != 0 and s1.high != 0:
+  if not (s1.isEmpty and s1.low == 0):
     result = (low: min(s1.low, s2.low),
               high: max(s1.high, s2.high))
   else:
