2fb6533c92cbeb74824d096d4b9fa2df3a7b2a6a
Vindaar <basti90@gmail.com>
2020-04-25 16:16:09 +0200

    update simpleFacet plot and add missing recipes

---
+++ recipes/rFacetTpa.nim
@@ -0,0 +1,12 @@
+import ggplotnim
+let df = toDf(readCsv("data/run_305_tpa_data.csv"))
+# gather all columns to a long format df
+echo df
+let dfLong = df.gather(getKeys(df), key = "Property", value = "Value")
+ggplot(dfLong, aes("Value")) +
+  facet_wrap("Property", 
+             scales = "free") + # each property has very different data ranges, Leave free
+  geom_histogram(bins = 100, position = "identity", 
+                 binBy = "subset") + # `binBy subset` means the histogram will be calculated 
+                                     # in the data range of each properties data range
+  ggsave("media/recipes/rFacetTpa.png", width = 800, height = 600)
---
+++ recipes/rNegativeBarPlot.nim
@@ -0,0 +1,11 @@
+import ggplotnim
+
+let trials = @["A", "B", "C", "D", "E"]
+let values = @[1.0, 0.5, 0, -0.5, -1.0]
+
+let df = seqsToDf({ "Trial" : trials,
+                    "Value" : values })
+
+ggplot(df, aes(x="Trial", y="Value")) +
+  geom_bar(stat="identity", position="identity") +
+  ggsave("media/recipes/rNegativeBarPlot.png")
