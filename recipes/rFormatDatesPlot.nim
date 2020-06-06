db3fe14ff5e8823eb9a78551eb699ed5ab216b0d
Vindaar <basti90@gmail.com>
2020-06-06 16:59:26 +0200

    fix DateTime recipe to get reproducible CI

--- recipes/rFormatDatesPlot.nim
+++ recipes/rFormatDatesPlot.nim
@@ -5,11 +5,14 @@ let y = x.mapIt(pow(sin(it), 2.0))
 
 let df = seqsToDf(x, y)
 
+# helper template to get a reproducible `DateTime` for CI!
+template nowTmpl(): untyped = initDateTime(15, mMay, 2020, 00, 00, 00, 00, utc())
+
 ggplot(df, aes("x", "y")) +
   geom_line() +
   scale_y_continuous(labels = proc(x: float): string =
                               x.formatFloat(ffDecimal, 1)) +
   scale_x_continuous(labels = proc(x: float): string =
-                              getDateStr(now() - int(x).months)) +
+                              getDateStr(nowTmpl() - int(x).months)) +
   xlab(label = " ") +
   ggsave("media/recipes/rFormatDatesPlot.png")
