6747daf7b35716cf529db3c968491f9f09c2cca0
Vindaar <basti90@gmail.com>
2020-07-24 20:56:53 +0200

    clean up multiple legends recipe

--- recipes/rMultipleLegends.nim
+++ recipes/rMultipleLegends.nim
@@ -1,5 +1,5 @@
 import ggplotnim
 let df = toDf(readCsv("data/mpg.csv"))
-ggplot(df, aes("cty", "displ", size = "cyl", color = "cty")) + #, color = "cty", size = "cyl")) +
+ggplot(df, aes("cty", "displ", size = "cyl", color = "cty")) +
   geom_point() +
   ggsave("media/recipes/rMultipleLegends.png")
