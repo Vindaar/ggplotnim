60116e3c76f4f03da8bb6e5efca4d63e104b64d0
Vindaar <basti90@gmail.com>
2020-07-24 20:59:17 +0200

    add recipe pngs and nim files

---
+++ recipes/rCustomMargins.nim
@@ -0,0 +1,15 @@
+import ggplotnim
+
+let
+  pos = [1, 2, 3, 1, 2, 3]
+  name = ["a very long long label", "a very long long label", "a very long long label", "b", "b", "b"]
+  n = [0, 1, 4, 4, 2, 3]
+  df = seqsToDf(pos, name, n)
+
+ggplot(df, aes("pos", "name")) +
+  geom_tile(aes(fill = "n")) +
+  geom_text(aes(text = "n"), size = some(25.0)) +
+  scale_x_discrete() +
+  scale_y_discrete() +
+  margin(left = 6.0) +
+  ggsave("media/recipes/rCustomMargins.png")
