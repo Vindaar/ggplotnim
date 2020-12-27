cca55fcb82d1627c1beaef5de0828d23ccdb7aa2
Vindaar <basti90@gmail.com>
2020-12-27 17:34:29 +0100

    deprecate `runRecipes` for compiling recipes, ⇒ `allRecipes.nim`
    `allRecipes.nim` will be added in a future commit and will import all
    recipes to cut down time to execute by replacing N compilations with 1

--- recipes/runRecipes.nim
+++ recipes/runRecipes.nim
@@ -16,15 +16,23 @@ macro genCommands(prefix: static string,
     if res[1] != 0:
       raise newException(Exception, "Failed to build or run at least one recipe!")
 
+template printWarning(): untyped =
+  echo "WARNING: Using `runRecipes` to compile and run all recipes is deprecated, "
+  echo "because it leads to extremely bad performance, due to the overhead of "
+  echo "compiling each module indepentently! Compile and run `allRecipes.nim` instead!"
+
 if paramCount() == 0:
   let t0 = epochTime()
+  printWarning()
   genCommands("nim c -r recipes/", ".nim")
   echo "Compilating and running all recipes took ", epochTime() - t0
 if paramCount() > 0:
   let p0 = paramStr(1)
   if p0 == "-c" or p0 == "--compile":
+    printWarning()
     genCommands("nim c recipes/", ".nim")
   elif p0 == "-cd" or p0 == "--compileDanger":
+    printWarning()
     genCommands("nim c -d:danger recipes/", ".nim")
   elif p0 == "-r" or p0 == "--run":
     let t0 = epochTime()
