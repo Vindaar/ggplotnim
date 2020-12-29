50db1d7f57e36f4d89d4a54e39e9cff868c8a033
Vindaar <basti90@gmail.com>
2020-12-29 12:07:17 +0100

    update pngs of expected plots

--- recipes/rAnnotatedHeatmap.nim
+++ recipes/rAnnotatedHeatmap.nim
@@ -4,6 +4,7 @@ let df = toDf(readCsv("data/mpg.csv"))
 let dfRed = df.group_by(["class", "cyl"]).summarize(f{float: "meanHwy" << mean( c"hwy" )})
 # stringification of formula is default name
 let meanHwyCol = "meanHwy"
+# fill only applies to `tile`, but not text. `color` would apply to text!
 ggplot(dfRed, aes("class", "cyl", fill = meanHwyCol)) +
   geom_tile() +
   geom_text(aes(text = meanHwyCol)) +
