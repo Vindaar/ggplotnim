5dd357989d165b68caae73d2a58acda0b5c977e3
Vindaar <basti90@gmail.com>
2021-08-28 16:21:24 +0200

    add `VegaTeX` helper file
    This file is used to avoid recursive imports.

---
+++ src/ggplotnim/ggplot_vegatex.nim
@@ -0,0 +1,13 @@
+from ../ggplotnim import ggsave, GgPlot, VegaTeX, VegaDraw
+import ggplot_vega
+export ggplot_vega
+
+proc `+`*(p: GgPlot, d: VegaTeX) =
+  ## Generates two plots. The TeX version and the Vega-Lite version.
+  # 1. generate the TeX version
+  p.ggsave(d.fname & ".tex", texOptions = d.texOptions)
+  # 2. generate the Vega version
+  let vegaDraw = VegaDraw(fname: d.fname & ".json", width: d.width,
+                          height: d.height,
+                          asPrettyJson: true)
+  p + vegaDraw
