a11f57d41be362f472e418245ca1f2a1a71915d8
Vindaar <basti90@gmail.com>
2021-11-27 13:55:07 +0100

    [tests] update coordinate of label in test after ginger placement change

--- tests/tests.nim
+++ tests/tests.nim
@@ -341,7 +341,7 @@ suite "GgPlot":
     # the default label margin is 1 cm, i.e. ~28.34 pixels at 72 dpi
     checkLabel(xLab[0], "xLabel", "x",
                Coord(x: Coord1D(pos: 0.5, kind: ukRelative),
-                     y: Coord1D(pos: 423.0944881889764, kind: ukPoint, length: some(pointHeight(plt.children[4])))),
+                     y: Coord1D(pos: 424.403937007874, kind: ukPoint, length: some(pointHeight(plt.children[4])))),
                none[float]())
     checkLabel(yLab[0], "yLabel", "exp",
                Coord(x: Coord1D(pos: -0.09393183283490754, kind: ukRelative),
