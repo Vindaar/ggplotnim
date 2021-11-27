8db4ab6ec06d1603d91a215969507108476507ed
Vindaar <basti90@gmail.com>
2021-11-27 15:06:07 +0100

    [tests] update tests & recipes to use explicit backend

--- recipes/rMultiSubplots.nim
+++ recipes/rMultiSubplots.nim
@@ -6,21 +6,23 @@ let df = seqsToDf({ "t" : t,
                     "Re x(t)" : y1,
                     "Im x(t)" : y2 })
 let plt1 = ggcreate(
-  ggplot(df, aes("t", "Re x(t)")) + 
+  ggplot(df, aes("t", "Re x(t)"),
+         backend = bkCairo) + # tell `ggsave` we wish to create the plot on the cairo backend
     geom_line() + 
     xlim(-0.02, 0.05) + 
     ggtitle("Real part of x(t)=e^{j 100 π t}"),
   width = 800, height = 300
 )
 let plt2 = ggcreate(
-  ggplot(df, aes("t", "Im x(t)")) + 
+  ggplot(df, aes("t", "Im x(t)"),
+         backend = bkCairo) +
     geom_line() + 
     xlim(-0.02, 0.05) + 
     ggtitle("Imaginary part of x(t)=e^{j 100 π t}"),
   width = 800, height = 300
 )
 # combine both into a single viewport to draw as one image
-var plt = initViewport(wImg = 800, hImg = 600)#wImg = 800.0, hImg = 800)
+var plt = initViewport(wImg = 800, hImg = 600, backend = bkCairo) # set backend of this viewport too
 plt.layout(1, rows = 2)
 # embed the finished plots into the the new viewport
 plt.embedAt(0, plt1.view)
