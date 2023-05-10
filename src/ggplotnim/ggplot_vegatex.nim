2862e8770299efe6304ef5ab74df4773cf4498f1
Vindaar <basti90@gmail.com>
2023-05-10 10:22:55 +0200

    [vega] update VegaDraw usage

--- src/ggplotnim/ggplot_vegatex.nim
+++ src/ggplotnim/ggplot_vegatex.nim
@@ -2,6 +2,8 @@ from ../ggplotnim import ggsave, GgPlot, VegaTeX, VegaDraw
 import ggplot_vega
 export ggplot_vega
 
+from std / options import some
+
 proc `+`*(p: GgPlot, d: VegaTeX) =
   ## Generates two plots. The TeX version and the Vega-Lite version.
   # 1. generate the TeX version
@@ -9,5 +11,5 @@ proc `+`*(p: GgPlot, d: VegaTeX) =
   # 2. generate the Vega version
   let vegaDraw = VegaDraw(fname: d.fname & ".json", width: d.width,
                           height: d.height,
-                          asPrettyJson: true)
+                          asPrettyJson: some(true))
   p + vegaDraw
